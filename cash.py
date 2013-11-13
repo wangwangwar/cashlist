323f4b06dc88f2e3262a90254c40a8031bcf988c
Wang <wangwangwar@gmail.com>
2013-11-13 22:08:35 +0800

    Clean
    Signed-off-by: Wang <wangwangwar@gmail.com>

--- cash.py
+++ cash.py
@@ -16,32 +16,11 @@ render = web.template.Render('templates', base='base')
 
 
 class Index:
-    form = web.form.Form(
-        web.form.Textbox('title', web.form.notnull,
-                         description=u"项目", placeholder=u"午餐"),
-        web.form.Textbox('yuan',
-                         web.form.regexp(r"\d+", u"必须是数字"),
-                         description=u"好多钱？", placeholder=u"10"),
-        web.form.Dropdown('type',
-                          [u'收入', u'支出'], value=u'支出',
-                          description=u"类型"),
-        web.form.Textbox("submit", html=u'添加'),
-    )
-
+    """Index page, """
     def GET(self):
         """Show page"""
         cash = model.get_cash_details()
-        form = self.form()
-        return render.index(cash, form)
-
-    def POST(self):
-        """Add new entry"""
-        form = self.form()
-        if not form.validates():
-            cash = model.get_cash_details()
-            return render.index(cash, form)
-        model.new_item(form.d.title, form.d.yuan, form.d.type)
-        raise web.seeother('/')
+        return render.index(cash)
 
 
 class Add:
